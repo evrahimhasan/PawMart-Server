5ae0e42604d7330ce7f384aa7d8a89f4aff8c995
evrahimhasan <evrahimhasanemran.com>
2025-12-06 21:11:43 +0600

    final commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Pass}@cluster0.83ib2ra.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.83ib2ra.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
